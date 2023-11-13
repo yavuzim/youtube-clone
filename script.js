0794c38a7aa31d200608a7ae10710555cc8b94bf
yavuzim <kyavuzimer@gmail.com>
2023-11-13 04:23:02 +0300

    anasayfa kısmı yapildi.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let menuIcon = document.querySelector(".menu-icon")
 let sideBar = document.querySelector(".sidebar")
+let container = document.querySelector(".container")
+
 
 menuIcon.onclick = function () {
     sideBar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
 }
+
+
